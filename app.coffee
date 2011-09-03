60e228ddcd66816b8481e91f29ec02301e412e9f
Jeffrey Basurto <jeffreybasurto@gmail.com>
2011-09-03 06:30:42 -0500

    first commit

---
+++ app.coffee
@@ -0,0 +1,31 @@
+mongoose = require "mongoose"
+_ = require "underscore"
+
+#mongoose.connect('mongodb://localhost/my_database');
+mongoose.connect 'mongodb:test:homework@dbh29.mongolab.com:27297/haiku'
+
+
+Schema = mongoose.Schema
+ObjectId = Schema.ObjectId
+
+Test = new Schema { title: String}
+mongoose.model('Test', Test)
+
+MyModel = mongoose.model 'Test'
+
+instance = new MyModel
+instance.title = 'hello'
+
+instance.save (err) ->
+  console.log("saving")
+
+MyModel.find {}, (err, docs) ->
+  _.each docs, (d)-> console.log(d)
+  
+  
+console.log(process.env.MONGOHQ_URL)
+
+
+  
+
+
---
+++ app.js
@@ -0,0 +1,24 @@
+(function() {
+  var MyModel, ObjectId, Schema, Test, instance, mongoose, _;
+  mongoose = require("mongoose");
+  _ = require("underscore");
+  mongoose.connect('mongodb:test:homework@dbh29.mongolab.com:27297/haiku');
+  Schema = mongoose.Schema;
+  ObjectId = Schema.ObjectId;
+  Test = new Schema({
+    title: String
+  });
+  mongoose.model('Test', Test);
+  MyModel = mongoose.model('Test');
+  instance = new MyModel;
+  instance.title = 'hello';
+  instance.save(function(err) {
+    return console.log("saving");
+  });
+  MyModel.find({}, function(err, docs) {
+    return _.each(docs, function(d) {
+      return console.log(d);
+    });
+  });
+  console.log(process.env.MONGOHQ_URL);
+}).call(this);
